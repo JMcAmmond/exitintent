ce7fb49695929302282968a70c380f3c0a6724fd
Jeff McAmmond <jeffmcammond@gmail.com>
2016-09-19 20:06:46 -0600

    Adding click events and animations

--- client/js/exitintent.js
+++ client/js/exitintent.js
@@ -8,12 +8,49 @@
             sensitivity : 20,
             disabled    : false,
             closeBtn    : true,
+            animation   : 'bounceIn',
             callback    : function() {}
         }, options);
 
         var _html = document.documentElement,
             _delayTimer = null,
             _self = this;
+
+        (function init() {
+            if(settings.closeBtn) {
+                _self.prepend('<span class="ei-close">&times;</span>')
+            }
+
+            if(settings.animation) {
+                _self.addClass("animated " + settings.animation);
+            }
+
+            _self.addClass('ei-content')
+                .wrap('<div class="ei-modal"></div>');
+
+            _self = $(_self).closest('.ei-modal');
+
+            attachClickEvents();
+
+            setTimeout(attachEvents, settings.timer);
+        })();
+
+        /**
+         * Attach click events to modal
+         */
+        function attachClickEvents() {
+            //Hide modal when clicking outside modal
+            _self.on('click', function() {
+                _self.hide();
+            }).children().click(function(e) {
+                return false;
+            });
+
+            //Hide modal when close button is clicked
+            _self.find('.ei-close').on('click', function() {
+                _self.hide();
+            });
+        }
 
         /**
          * Attaches events to the window and close modal events
@@ -23,10 +60,6 @@
 
             _html.addEventListener('mouseleave', handleMouseleave);
             _html.addEventListener('mouseenter', handleMouseenter);
-
-            _self.find('.ei-close').on('click', function() {
-                _self.hide();
-            });
         }
 
         /**
@@ -49,11 +82,28 @@
             }
         }
 
+        function centerElement(el) {
+            el.css({
+                'position': 'absolute',
+                'left': '50%',
+                'top': '50%',
+                'z-index': 10000
+            });
+
+            el.css({
+                'margin-left': -el.outerWidth() / 2 + 'px',
+                'margin-top': -el.outerHeight() / 2 + 'px'
+            });
+        }
+
         /**
          * Fires the exit intent modal
          */
         function fire() {
             if( isDisabled() ) { return; }
+
+            _self.show();
+            centerElement(_self.find('.ei-content'));
 
             settings.callback();
             disable();
@@ -76,25 +126,6 @@
         function isDisabled() {
             return settings.disabled;
         }
-
-        /**
-         * Wraps content and in '.ei-modal' div and prepend close button
-         */
-        (function wrapContent() {
-            _self.addClass('ei-content')
-                .wrap('<div class="ei-modal"></div>');
-
-            if(settings.closeBtn) {
-                _self.prepend('<span class="ei-close">&times;</span>')
-            }
-
-            _self = $(_self).closest('.ei-modal');
-        })();
-
-        /**
-         * Attach Exit Intent after allotted time
-         */
-        setTimeout(attachEvents, settings.timer);
 
         /**
          * Returned functions
